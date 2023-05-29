68d6e51f180d74f41ca2a9944cdd2f79247baff6
Brahim <32096282+bra1438@users.noreply.github.com>
2023-05-29 08:47:26 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -107,7 +107,7 @@ def main():
     st.subheader('About this app')
     st.markdown("""
     The app uses an implementation of LeNet-5 Convolutional Neural Network. 
-    The model was trained and tested on about 40.000 real photos of 43 types of german traffic signs.
+    The model was trained and tested on about 40.000 real photos of 43 types of traffic signs.
     
   
     
